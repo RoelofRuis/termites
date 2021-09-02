eb44a824ca1b6a531404ede3882106e1aa633df2
Roelof Ruis <roelof@pbwebmedia.com>
2021-09-02 11:11:11 +0200

    Simple ID implementation

---
+++ termites-core/id.go
@@ -0,0 +1,36 @@
+package termites
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+// TODO: usage of UUID can be removed when using this everywhere
+
+type ObjectName string
+
+type Identifier struct {
+	ObjName ObjectName
+	Id      string
+}
+
+func (o Identifier) String() string {
+	return fmt.Sprintf("%s-%s", o.ObjName, o.Id)
+}
+
+func NewIdentifier(n ObjectName) Identifier {
+	return Identifier{
+		ObjName: n,
+		Id:      randomId(),
+	}
+}
+
+var alphabet = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
+
+func randomId() string {
+	b := make([]rune, 16)
+	for i := range b {
+		b[i] = alphabet[rand.Intn(len(alphabet))]
+	}
+	return string(b)
+}
